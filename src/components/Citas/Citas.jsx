3fd2f3208e18ad94d572907ceacd8200519c3ee8
andres0818 <c.andres.perez.h@gmail.com>
2023-05-04 14:38:43 -0500

    create user in firebase

--- src/components/Citas/Citas.jsx
+++ src/components/Citas/Citas.jsx
@@ -1,28 +1,65 @@
-import React from "react";
-import './Citas.scss'
+import React, { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
+
+import "./Citas.scss";
+import { db } from "../../Firebase/firebase";
 
 const Citas = () => {
+  const [data, setData] = useState({});
+
+  const handlerChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const createUser = async (e) => {
+    e.preventDefault();
+    await setDoc(doc(db, "patient", data.identificacion), {
+      name: data.nombre,
+      identificacion: data.identificacion,
+      edad: data.edad,
+      email: data.email,
+      direccion: data.direccion,
+      fechaNacimiento: data.fechaNacimineto,
+      celular: data.celular,
+      fechaIngreso: data.fechaIngreso,
+      contactoEmergencia: data.contactoEmergencia,
+      valorConsulta: data.valorConsulta,
+      profesional: data.profesional,
+    })
+      .then(() => console.log("usuario creado"))
+      .catch((err) => console.log(err.message));
+  };
+
   return (
     <div className="px-12">
       <h1 className="text-center text-6xl font-bold text-green-600">
         Ingreso Pacientes
       </h1>
-      <form>
+      <form onSubmit={(e) => createUser(e)}>
         <h2 className="text-2xl font-bold text-green-600 mb-4">
           Informacion Personal
         </h2>
         <div className="flex flex-row justify-between">
           <input
+            onChange={handlerChange}
+            required
+            name="nombre"
             className="shadow-xl bg-gray-100 w-1/3 rounded-md text-2xl m-4 px-4 py-3"
             type="text"
             placeholder="Nombre"
           />
           <input
+            onChange={handlerChange}
+            required
+            name="identificacion"
             className="shadow-xl bg-gray-100 w-1/3 rounded-md text-2xl m-4 px-4 py-3"
             type="text"
             placeholder="Identificación"
           />
           <input
+            onChange={handlerChange}
+            required
+            name="edad"
             className="shadow-xl bg-gray-100 w-1/3 rounded-md text-2xl m-4 px-4 py-3"
             type="text"
             placeholder="Edad"
@@ -30,17 +67,25 @@ const Citas = () => {
         </div>
         <div className="flex flex-row justify-between">
           <input
+            onChange={handlerChange}
+            required
+            name="email"
             className="shadow-xl bg-gray-100 w-1/3 rounded-md text-2xl m-4 px-4 py-3"
             type="email"
             placeholder="Email"
           />
           <input
+            onChange={handlerChange}
+            required
+            name="direccion"
             className="shadow-xl bg-gray-100 w-1/3 rounded-md text-2xl m-4 px-4 py-3"
             type="text"
             placeholder="Dirección"
           />
           <input
-          name="nacimiento"
+            onChange={handlerChange}
+            required
+            name="fechaNacimineto"
             className="shadow-xl bg-gray-100 w-1/3 rounded-md text-2xl m-4 px-4 py-3"
             type="date"
             placeholder="Fecha de Nacimiento"
@@ -48,17 +93,25 @@ const Citas = () => {
         </div>
         <div className="flex flex-row justify-between">
           <input
+            onChange={handlerChange}
+            required
+            name="celular"
             className="shadow-xl bg-gray-100 w-1/3 rounded-md text-2xl m-4 px-4 py-3"
             type="text"
             placeholder="Celular"
           />
           <input
-          name="ingreso"
+            onChange={handlerChange}
+            required
+            name="fechaIngreso"
             className=" shadow-xl bg-gray-100 w-1/3 rounded-md text-2xl m-4 px-4 py-3"
             type="date"
             placeholder="Fecha de Ingreso"
           />
           <input
+            onChange={handlerChange}
+            required
+            name="contactoEmergencia"
             className="shadow-xl bg-gray-100 w-1/3 rounded-md text-2xl m-4 px-4 py-3"
             type="text"
             placeholder="Contacto de Emergencia"
@@ -70,19 +123,29 @@ const Citas = () => {
         </h1>
         <div className="flex flex-row ">
           <input
+            onChange={handlerChange}
+            name="valorConsulta"
             className="shadow-xl bg-gray-100 w-1/3  rounded-md text-2xl m-4 px-4 py-3"
             type="text"
             placeholder="Valor Consulta"
           />
           <input
+            onChange={handlerChange}
+            name="profesional"
             className="shadow-xl bg-gray-100 w-1/3  rounded-md text-2xl m-4 px-4 py-3"
             type="text"
             placeholder="Profesional"
           />
-          <input className="w-1/3 m-4 px-4 py-3" disabled />
+          <input
+            className="w-1/3 m-4 px-4 py-3"
+            disabled
+          />
         </div>
         <div className="flex items-center justify-center">
-          <button className="bg-green-600 mt-4 px-8  py-3 rounded-3xl text-4xl font-bold text-white flex justify-center items-center">
+          <button
+            type="submit"
+            className="bg-green-600 mt-4 px-8  py-3 rounded-3xl text-4xl font-bold text-white flex justify-center items-center"
+          >
             Guardar
           </button>
         </div>
