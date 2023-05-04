7f7cb63174e9d1ecfd903f0a8a23828a98fc25e8
andres0818 <c.andres.perez.h@gmail.com>
2023-05-04 15:48:29 -0500

    change name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Consultorios from "./components/Consultorios/Consultorios";
 import NavBar from "./components/NavBar/NavBar";
-import Citas from "./components/Citas/Citas";
+import Citas from "./components/Registrar/Registrar";
+import Registrar from "./components/Registrar/Registrar";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<NavBar />}>
         <Route path="/" element={<Consultorios />} />
-        <Route path="/citas" element={<Citas />} />
+        <Route path="/registrar" element={<Registrar />} />
       </Route>
     </Routes>
   );
