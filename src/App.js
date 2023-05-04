3d89c4db0d6c02f26d864f130976f9b119371049
andres0818 <c.andres.perez.h@gmail.com>
2023-05-04 11:33:18 -0500

    finsh component for consultorios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Consultorios from "./components/Consultorios/Consultorios";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Consultorios />} />
+      <Route path="/" element={<NavBar />}>
+        <Route path="/" element={<Consultorios />} />
+        <Route path="/citas" element={<Consultorios />} />
+      </Route>
     </Routes>
   );
 }
