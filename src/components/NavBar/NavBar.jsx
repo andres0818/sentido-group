eb817464a7e62280dccf1e80e0018c093dfe0980
andres0818 <c.andres.perez.h@gmail.com>
2023-05-08 22:01:22 -0500

    change text

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
         <img className="cursor-pointer" src={logo} alt="Sentido Group" onClick={()=>navigate('/')}/>
         <div className="flex items-center justify-center gap-10">
           <Link className="text-green-600 font-bold text-2xl" to={'/'}>Consultorios</Link>
-          <Link className="text-green-600 font-bold text-2xl" to={'/registrar'}>Registrar</Link>
+          <Link className="text-green-600 font-bold text-2xl" to={'/registrar'}>Ingreso Pacientes</Link>
           <Link className="text-green-600 font-bold text-2xl" to={'/citas'}>Citas</Link>
             <img className="w-12 h-12" src={login} alt="" />
         </div>
