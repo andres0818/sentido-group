48c1a83380a03ca36e9ca7b8581931ca132db195
andres0818 <c.andres.perez.h@gmail.com>
2023-05-08 21:56:27 -0500

    add loader

--- src/components/Citas/Cards.jsx
+++ src/components/Citas/Cards.jsx
@@ -9,19 +9,19 @@ const Cards = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-ful justify-end  flex-wrap flex ">
-      {data ? (
+    <div className="w-ful flex-wrap flex ">
+      {data?.length > 0 ? (
         data.map((e, index) => {
           const { name, valorConsulta, profesional, userId, fecha } = e;
           return (
             <div
               key={index}
               onClick={() => navigate(`/citas/${userId}`)}
-              className="bg-green-600/60 max-w-xs p-4 w-1/3 m-5 rounded-xl"
+              className="bg-green-600/60 max-w-xs  p-4 w-1/3 m-1 rounded-xl"
             >
               <h1 className=" font-bold mb-4 text-3xl text-center">{name}</h1>
               <div className="flex mb-1 flex-wrap ">
-                <h2 className="w-1/2 text-xl font-bold">Profesional:</h2>
+                <h2 className="w-1/2 text-xl font-bold">Psicólogo:</h2>
                 <h2 className="w-1/2 text-xl">{profesional}</h2>
               </div>
               <div className="flex mb-1 flex-wrap ">
@@ -36,18 +36,12 @@ const Cards = () => {
           );
         })
       ) : (
-        <LoadingSpinner />
+        <h1 className=" mt-12 text-gray-500 text-3xl text-center w-full">
+          Resultado no encontrado
+        </h1>
       )}
     </div>
   );
 };
 
 export default Cards;
-
-export function LoadingSpinner() {
-  return (
-    <div className="spinner-container">
-      <div className="loading-spinner"></div>
-    </div>
-  );
-}
