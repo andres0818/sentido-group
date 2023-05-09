e3b9ee06ae0a43741f2deb62c30797361d71c224
andres0818 <c.andres.perez.h@gmail.com>
2023-05-09 09:12:29 -0500

    ok

---
+++ src/components/User/History.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../Firebase/firebase";
+import { useParams } from "react-router-dom";
+
+const History = ({ user }) => {
+  const [data, setData] = useState([]);
+
+  const { id } = useParams();
+
+  async function buscarEnFirebase() {
+    const q = query(collection(db, "diary"), where("userId", "==", id));
+    const querySnapshot = await getDocs(q);
+    // Devolver los resultados de la consulta
+    const nuevosDatos = querySnapshot.docs.map((doc) => doc.data());
+    setData(nuevosDatos);
+  }
+
+  console.log(data)
+
+  useEffect(() => {
+    buscarEnFirebase();
+  }, []);
+
+  return     <div className="w-ful flex-wrap flex p-10">
+  {data?.length > 0 ? (
+    data.map((e, index) => {
+      const { name, valorConsulta, profesional, fecha } = e;
+      return (
+        <div
+          key={index}
+          className="bg-green-600/60 max-w-xs  p-4 w-1/3 m-1 rounded-xl"
+        >
+          <h1 className=" font-bold mb-4 text-3xl text-center">{name}</h1>
+          <div className="flex mb-1 flex-wrap ">
+            <h2 className="w-1/2 text-xl font-bold">Psicólogo:</h2>
+            <h2 className="w-1/2 text-xl">{profesional}</h2>
+          </div>
+          <div className="flex mb-1 flex-wrap ">
+            <h2 className="w-1/2 text-xl font-bold">Valor:</h2>
+            <h2 className="w-1/2 text-xl">${valorConsulta}</h2>
+          </div>
+          <div className="flex mb-1 flex-wrap ">
+            <h2 className="w-1/2 text-xl font-bold">Fecha</h2>
+            <h2 className="w-1/2 text-xl">{fecha}</h2>
+          </div>
+        </div>
+      );
+    })
+  ) : (
+    <h1 className=" mt-12 text-gray-500 text-3xl text-center w-full">
+      Resultado no encontrado
+    </h1>
+  )}
+</div>;
+};
+
+export default History;
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -4,6 +4,7 @@ import { addDoc, collection, doc, getDoc } from "firebase/firestore";
 import { db } from "../../Firebase/firebase";
 import { Profile } from "./Profile";
 import NewDiary from "./NewDiary";
+import History from "./History";
 
 const User = () => {
   const { id } = useParams();
@@ -24,21 +25,32 @@ const User = () => {
     getUser(id);
   }, [id]);
 
-  const handlerForm = ({ btn = "cita" }) => {
+  const handlerForm = () => {
     setIsForm({ ...user, diary: !isForm.diary });
   };
 
+  const handlerHistory= () => {
+    setIsForm({ ...user, list: !isForm.list });
+  };
   return (
     <>
       <Profile user={user} />
-      <div className="flex justify-center">
+      <div className="flex mt-4 justify-center">
         <button
           onClick={handlerForm}
           className="w-1/3 max-w-sm shadow-xl rounded-md text-2xl m-4 px-4 py-3 bg-green-500 hover:bg-green-600"
         >
           Nueva Cita
         </button>
+        <button
+          onClick={handlerHistory}
+          className="w-1/3 max-w-sm shadow-xl rounded-md text-2xl m-4 px-4 py-3 bg-green-500 hover:bg-green-600"
+        >
+         Registro
+        </button>
       </div>
+      
+      {isForm.list && <History />}
       {isForm.diary && <NewDiary user={user} />}
     </>
   );
