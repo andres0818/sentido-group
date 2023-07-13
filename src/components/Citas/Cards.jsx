4a4fbc5766e4510ad7816c9d2c2b8d5b3d6c30c1
andres0818 <c.andres.perez.h@gmail.com>
2023-07-12 21:29:49 -0500

    change style

--- src/components/Citas/Cards.jsx
+++ src/components/Citas/Cards.jsx
@@ -72,14 +72,14 @@ const Cards = () => {
         </div>
       <div className="flex justify-center mt-4">
         <button
-          className="px-4 py-2 mr-2 bg-gray-200 rounded"
+          className="px-4 py-2 mr-2 font-semibold bg-gray-200 rounded"
           onClick={goToPreviousPage}
           disabled={currentPage === 1}
           >
           Anterior
         </button>
         <button
-          className="px-4 py-2 bg-gray-200 rounded"
+          className="px-4 py-2 font-semibold bg-gray-200 rounded"
           onClick={goToNextPage}
           disabled={currentPage === totalPages}
           >
