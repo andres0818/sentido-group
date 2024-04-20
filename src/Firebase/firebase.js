2402af581f3535980b18bdacfdff34c8af9a975b
andres0818 <c.andres.perez.h@gmail.com>
2024-04-20 10:20:56 -0500

    se configuran variables de entorno

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARNEnNNT2EW5HMk-JHIHyUIz-NqlqOn2M",
-  authDomain: "sentido-group.firebaseapp.com",
-  projectId: "sentido-group",
-  storageBucket: "sentido-group.appspot.com",
-  messagingSenderId: "745854997698",
-  appId: "1:745854997698:web:c6e8e0bea871f1ab4c175b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore();
